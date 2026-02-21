ab1a621b36b1a2083ef6be043e6ddcebeda70d71
JanelleW03 <115589579+JanelleW03@users.noreply.github.com>
2026-02-21 17:00:34 -0500

    Updating UI to new Unity system

--- Voyage of the Fallen Star/Assets/DialogueManager.cs
+++ Voyage of the Fallen Star/Assets/DialogueManager.cs
@@ -1,66 +1,22 @@
-using System.Collections;
+using NUnit.Framework;
 using UnityEngine;
-using TMPro;
-using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class DialogueManager : MonoBehaviour
 {
-    public TextMeshProUGUI textComponent;
-    public string[] lines;
-    public float textSpeed;
-
-    private InputAction _continueAction;
-    private int _index;
+    public static UnityEvent<DialogueField> NPCSpeaking = new UnityEvent<DialogueField>();
+    DialogueField TextRef;
+    // List<AudioClip>;
     
     private void Start()
     {
-        textComponent.text = string.Empty;
-        _continueAction = InputSystem.actions.FindAction("Continue");
-        StartDialogue();
+       
     }
 
     private void Update()
     {
-        if (_continueAction.WasPerformedThisFrame())
-        {
-            if(textComponent.text == lines[_index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text  = lines[_index];
-            }
-        }
+       
     }
-    private void StartDialogue()
-    {
-        _index = 0;
-        StartCoroutine(TypeLine());
-    }
-
-    private IEnumerator TypeLine()
-    {
-        foreach (var c in lines[_index])
-        {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
-    private void NextLine()
-    {
-        if(_index < lines.Length - 1)
-        {
-            _index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
-        }
-        else
-        {
-            gameObject.transform.parent.gameObject.SetActive(false);
-        }
-    }
+    
 }
 
